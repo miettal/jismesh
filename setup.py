2467b814f18374324587fdc1d7db2e0145afbdc0
hni14 <you@example.com>
2016-03-13 21:05:37 +0900

    pypi meta information update.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 import sys
 
 setup(name='jismesh',
-      version='0.1',
+      version='0.1.1',
       description='Utilities for area mesh code definted in Japanese Industorial Standards (JIS X 0410 地域メッシュコード).',
       author='Haruki Nishikawa',
       author_email='harukinishikawa84@hotmail.com',
-      url='',
+      url='https://github.com/hni14/jismesh',
       packages=['jismesh'],
      )
